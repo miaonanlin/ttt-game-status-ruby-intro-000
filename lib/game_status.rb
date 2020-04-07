40c4179f637dd9f812c5e3e382128c8131d0c932
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-07 20:31:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ WIN_COMBINATIONS = [
        position_3 = board[win_index_3]
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination 
+        return win_combination
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
       end
@@ -30,8 +30,6 @@ WIN_COMBINATIONS = [
 end
 
 def full?(board)
-  board.all? 
+  board.all?
   {|index| index == "X" || index =="O"}
-end 
-
-
+end
